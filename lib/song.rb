9a24829efd751a48a9b2eed693c0f2498ee126de
Brian Matheny <bwmatheny@gmail.com>
2020-05-19 17:25:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,10 +34,9 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    @@all.find {|i| i.name == name}
-    if false
-      self.create_by_name
-    end
+    if self.find_by_name(name)
+    else
+      self.create_by_name(name)
   end
 
 end
